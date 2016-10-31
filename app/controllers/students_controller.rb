4f13c9a1ce9494b637a14e48b8d2393c4a544bd1
Kyle Geske <stungeye@gmail.com>
2016-10-31 15:30:15 -0500

    Implemented the student#edit and student#update.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,7 @@ class StudentsController < ApplicationController
   # Automatically load the app/views/students/index.html.erb
 
   def show
-    @student = Student.find(params[:id]) # Load student mentioned by id in the route.
+    @student = find_student
   end
   # Automatically load the app/views/students/show.html.erb
 
@@ -25,7 +25,27 @@ class StudentsController < ApplicationController
   end
   # This action does not have an associated view!
 
+  def edit
+    @student = find_student
+  end
+  # Automatically loads the associated view app/views/students/edit.html.erb
+
+  def update
+    @student = find_student
+
+    if @student.update(student_params) # If validations pass
+      redirect_to @student # Redirect to the show action for this student.
+    else # if the validations fail
+      render :edit # Render the edit view to show the form again and display validation errors.
+    end
+  end
+  # No view associated with this action we either redirect or render.
+
   private
+
+  def find_student
+    Student.find(params[:id]) # Load student mentioned by id in the route.
+  end
 
   def student_params
     params.require(:student).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,9 @@ Rails.application.routes.draw do
   get  'students/new' => 'students#new', as: 'new_student'
   post 'students'     => 'students#create'
 
+  # UPDATE Routes
+  get 'students/:id/edit' => 'students#edit', as: 'edit_student', id: /\d+/
+  patch 'students/:id'    => 'students#update'
+
   root to: 'students#index'
 end
